aadce3b6b996c165b4a02007c9457d94a2198520
gatolgaj <gatolgaj@gmail.com>
2024-04-25 15:58:28 +0200

    Clean Code

--- SolveWithGoogle.py
+++ SolveWithGoogle.py
@@ -1,35 +1,20 @@
 import os
-import sqlite3
-import sys
 import chainlit as cl
 from chainlit.types import ThreadDict
 from langchain_community.vectorstores import Qdrant
 from qdrant_client import QdrantClient
-from langchain.agents import Tool, initialize_agent, AgentType
-from langchain_openai import OpenAIEmbeddings
-from langchain_openai import ChatOpenAI
-from typing import Dict, Optional
-from langchain.chat_models import ChatOpenAI
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
-from langchain.chains import RetrievalQA
-from langchain.agents import Tool
 from langchain.agents import initialize_agent
-from langchain.sql_database import SQLDatabase
-from langchain_community.agent_toolkits import create_sql_agent
+from langchain_openai import OpenAIEmbeddings
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.agents import initialize_agent
 from langchain_core.tools import tool
-import sqlite3
-import plotly.express as px
-import pandas as pd
-from langchain.chains import GraphCypherQAChain
-from langchain_community.graphs import Neo4jGraph
 from langchain.prompts.prompt import PromptTemplate
 from langchain_google_vertexai import VertexAI
 import fitz  # PyMuPDF
 
 model = VertexAI(model_name="gemini-1.5-pro-preview-0409")
 
-# message = "What are some of the pros and cons of Python as a programming language?"
-# print(model.invoke(message))
+
 
 
 
@@ -52,27 +37,6 @@ def retrieve_patient_data(query):
     profiles = vector_store_p.similarity_search(query, k=5)
     data = [profile.page_content for profile in profiles]
     return ' '.join(data)
-
-def read_pdf(file_path):
-    """
-    Reads a PDF and returns its text content.
-
-    Args:
-    file_path (str): The path to the PDF file to be read.
-
-    Returns:
-    str: The text content of the PDF.
-    """
-    text = ''
-    # Open the provided PDF file
-    with fitz.open(file_path) as doc:
-        # Iterate over each page
-        for page in doc:
-            # Extract text from each page and append it to the text variable
-            text += page.get_text()
-
-    return text
-
 
 
 
@@ -108,32 +72,10 @@ def patient_query(query):
     context = retrieve_patient_data(query)
     return chain.invoke({"question": query , "context": context})
 
-@tool
-def teen_pdf_query(query):
-    """This is a tool used to answer questions for teens. Use this tool for all the questions related to teens.
-    """
 
-    template = """
-    Context : {context}
-    Question: {question}
-
-    """
-    prompt = PromptTemplate.from_template(template)
-
-    chain = prompt | model
-
-    context = retrieve_pdf_data(query)
-    return chain.invoke({"question": query , "context": context})
 
 tools = [pdf_query,patient_query]
 
-
-# print(agent.agent.llm_chain.prompt)
-# print(agent(query))
-# print(agent(query2))
-# #print(agent(query2_5))
-# print(agent(query3))
-# print(agent(query4))
 
 welcome_message = ("Welcome to the Princess Maxima Patient Care Center. We’re here to support you "
                    "and your loved ones every step of the way. If you have any questions or need "
@@ -142,16 +84,11 @@ welcome_message = ("Welcome to the Princess Maxima Patient Care Center. We’re 
 
 @cl.on_chat_start
 async def  start():
-    #tools = load_tools(["requests_all"])
-    #app_user = cl.user_session.get("user")
-    #cl.Message(f"Hello {app_user.identifier}").send()
+
     print("A new chat session has started!")
 
     await cl.Message(content=welcome_message).send()
-    # agent = initialize_agent(
-    #     tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
 
-    # )
     conversational_memory = ConversationBufferWindowMemory(
     memory_key='chat_history',
     k=10,
@@ -169,33 +106,10 @@ async def  start():
     )
     cl.user_session.set("agent", agent)
 
-# @cl.oauth_callback
-# def oauth_callback(
-#         provider_id: str,
-#         token: str,
-#         raw_user_data: Dict[str, str],
-#         default_user: cl.User,
-# ) -> Optional[cl.User]:
-#   return default_user
 
 @cl.on_message
 async def main(message: cl.Message):
-    #agent = cl.user_session.get("agent")  # type: AgentExecutor
-    #print(agent)
-    # agent = initialize_agent(
-    #     tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
-    # )
-    # res = await agent.run(
-    #     message.content, callbacks=[cl.AsyncLangchainCallbackHandler()]
-    # )
 
-    #     for chunk in await cl.make_async(runnable.stream)(
-    #     {"question": message.content},
-    #     config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
-    # ):
-    #     await msg.stream_token(chunk)
-
-    # await msg.send()
     agent = cl.user_session.get("agent") 
     cl.user_session.set("figure", None)
     res = await cl.make_async(agent.run)(
@@ -216,9 +130,6 @@ async def on_action(action: cl.Action):
 
     return "Thank you for clicking on the action button!"
 
-# @cl.on_chat_start
-# # def on_chat_start():
-# #     print("A new chat session has started!")
 
 @cl.on_stop
 def on_stop():
